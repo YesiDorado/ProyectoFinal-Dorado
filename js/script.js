ba3be155fc36b57abe2229ba82986e29174d0e72
YesiDorado <doradoyesica@gmail.com>
2023-04-15 17:01:23 -0300

    final

--- js/script.js
+++ js/script.js
@@ -103,12 +103,15 @@ productos.forEach((product) => {
 });
 
 verCarrito.addEventListener("click", () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
     const modalHeader = document.createElement("div");
     modalHeader.className = "modal-header"
     modalHeader.innerHTML = `
         <h1 class="modal-header-title">Carrito.</h1>
     `;
     modalContainer.append(modalHeader);
+
     const modalbutton = document.createElement("h1");
     modalbutton.innerText = "x";
     modalbutton.className = "modal-header-button";
@@ -116,22 +119,22 @@ verCarrito.addEventListener("click", () => {
     modalbutton.addEventListener("click", () => {
         modalContainer.style.display = "none";
     });
-    
+
     modalHeader.append(modalbutton);
 
     carrito.forEach((product) => {
-    let carritoContent = document.createElement("div")
-    carritoContent.className = "modal-content"
-    carritoContent.innerHTML = `
+        let carritoContent = document.createElement("div")
+        carritoContent.className = "modal-content"
+        carritoContent.innerHTML = `
         <img src="${product.img}">
         <h3>${product.nombre}</h3>
         <p>${product.precio} $</p>
     `;
 
-    modalContainer.append(carritoContent)
+        modalContainer.append(carritoContent)
     });
 
-    const total =carrito.reduce((acc, el) => acc + el.precio, 0);
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
 
     const totalBuying = document.createElement("div")
     totalBuying.className = "total-content"
